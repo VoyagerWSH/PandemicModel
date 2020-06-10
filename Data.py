98ee112f25034af6e8e8b7bd1281a61cdcf3ec63
swadhwa5 <swadhwa5@jh.edu>
2020-06-10 06:23:36 +0530

    can read csv file

--- Data.py
+++ Data.py
@@ -1,5 +1,5 @@
-import pandas
-import geopandas
-
+import pandas as pd
 # get data from google Maps Place API about categorisation of establishments and store as JSON File
+data = pd.read_csv("~/Desktop/Shreya/JHU/Summer/PandemicModel/data.csv")
+print(data.head())
 
