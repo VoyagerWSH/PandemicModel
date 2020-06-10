735d1972a43d081e32a8b2ef1cc263ccd9c429df
swadhwa5 <swadhwa5@jh.edu>
2020-06-11 04:38:44 +0530

    removed api key

--- Data.py
+++ Data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 # get data from google Maps Place API about categorisation of establishments and store as JSON File
-data = pd.read_csv("~/Desktop/Shreya/JHU/Summer/PandemicModel/data.csv")
+data = pd.read_csv("sampleData.csv")
 print(data.head())
 
