1bf86cbd357a84d05d32cec041a4fd5fb252a2fb
swadhwa5 <swadhwa5@jh.edu>
2020-06-11 03:59:06 +0530

    python version of Map_with_Visualisation.html, reading csv properly, but not displaying markers properly

---
+++ Map_with_Visualizations.py
@@ -0,0 +1,19 @@
+from bokeh.io import output_file, show
+from bokeh.models import Circle, ColumnDataSource, GMapOptions
+from bokeh.plotting import gmap
+import pandas as pd
+
+output_file("gmap.html")
+data = pd.read_csv("sampleData.csv")
+lats = data['Lat']
+longs = data['Long']
+
+print(data['Lat'])
+
+map_options = GMapOptions(lat=33.03544, lng=-111.644, map_type="roadmap", zoom=2)
+p = gmap("API_KEY", map_options, title="Covid Map")
+
+#p.circle(x="lats", y="longs", size=15, fill_color="blue", fill_alpha=0.8)
+#glyph = Circle(x="lats", y="longs")
+#p.add_glyph(glyph)
+show(p)
