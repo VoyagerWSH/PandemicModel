988c9bb30eed6af7bc74463a3dc7955e3f67b436
swadhwa5 <swadhwa5@jh.edu>
2020-06-14 01:23:45 +0530

    javascript code for index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Initialize and add the map
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8,-187.3), mapTypeId: 'roadmap'
+    });
+}
