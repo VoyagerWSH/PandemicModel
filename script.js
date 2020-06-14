385a4140344eaa6a61d22ecac41f67ca8ef0635e
swadhwa5 <swadhwa5@jh.edu>
2020-06-14 20:06:28 +0530

    json file being produced, connect input from index.html city dropdown to input_city variable

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(2.8,-187.3), mapTypeId: 'roadmap'
     });
 }
+
+var input_city = "Barnsdall";//this input should come from the dropdown in index.html select city
+city_data = "https://maps.googleapis.com/maps/api/place/findplacefromtext/jsonp?input=".concat(input_city);
+city_data = city_data.concat("&inputtype=textquery&fields=formatted_address,geometry,place_id,type&key=YOUR_API_KEY")
+console.log(city_data);//this is the final link for the json data file from which you can extract the bounding box coordinates 
+  
