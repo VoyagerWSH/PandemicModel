8682bb9eaed6b815f063ad555011ed78a536f569
swadhwa5 <swadhwa5@jh.edu>
2020-06-18 14:02:11 +0530

    functionality to return details of a place added, and returns all details as strings, also can create markers

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 
+var input_city = 'Barnsdall';//this input should come from the dropdown in index.html select city
+
 // Initialize and add the map
 function initMap() {
 map = new google.maps.Map(document.getElementById('map'), {
     zoom: 2,
     center: new google.maps.LatLng(2.8,-187.3), mapTypeId: 'roadmap'
     });
+
+infowindow = new google.maps.InfoWindow();
+
+var request = {
+    query: input_city,
+    fields: ['name', 'formatted_address', 'geometry', 'place_id' , 'types'],
+};
+
+var service = new google.maps.places.PlacesService(map);
+var id;
+var location;
+var north_east;
+var south_west;
+
+service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        location = results[i].geometry.location;
+        north_east = results[i].geometry.viewport.getNorthEast();//northeast bound
+        south_west = results[i].geometry.viewport.getSouthWest();//southwest bound
+        console.log(location.toString());//just for reference
+        console.log(north_east.toString());//just for reference
+        console.log(south_west.toString());//just for reference
+        createMarker(results[i]);
+      }
+    }
+});
 }
 
-var input_city = "Barnsdall";//this input should come from the dropdown in index.html select city
-city_data = "https://maps.googleapis.com/maps/api/place/findplacefromtext/jsonp?input=".concat(input_city);
-city_data = city_data.concat("&inputtype=textquery&fields=formatted_address,geometry,place_id,type&key=YOUR_API_KEY")
-console.log(city_data);//this is the final link for the json data file from which you can extract the bounding box coordinates 
-  
+function createMarker(place, place_type) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+      'Type: ' + place.types + '</div>');
+      infowindow.open(map, this);
+    });
+}
