38efb19c14cdf12bafca3aa62928dcd0e92fe8ea
swadhwa5 <swadhwa5@jh.edu>
2020-06-24 17:37:48 +0530

    added dropdown menu and cleaned up code

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
-function initAutocomplete() {
+var markers = [];
+var map;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    initMap();
+});
+
+function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 48,
@@ -9,17 +16,15 @@ function initAutocomplete() {
         disableDefaultUI: true
     });
 
+    choseLocationAutocomplete();
+}
+
+function choseLocationAutocomplete() {
     infowindow = new google.maps.InfoWindow();
     // Create the search box and link it to the UI element.
     var input = document.getElementById('input');
     var autocomplete = new google.maps.places.Autocomplete(input);
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
-
-    var place;
-    var id;
-    var location;
-    var north_east;
-    var south_west;
 
     // Set the data fields to return when the user selects a place.
     autocomplete.setFields(
@@ -28,7 +33,7 @@ function initAutocomplete() {
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     autocomplete.addListener('place_changed', function () {
-        place = autocomplete.getPlace();
+        var place = autocomplete.getPlace();
         if (!place.geometry) {
             console.log("Returned place contains no geometry");
             return;
@@ -41,47 +46,44 @@ function initAutocomplete() {
             bounds.extend(place.geometry.location);
         }
         map.fitBounds(bounds);
-        id = place.place_id;
-        location = place.geometry.location;
-        north_east = place.geometry.viewport.getNorthEast();//northeast bound
-        south_west = place.geometry.viewport.getSouthWest();//southwest bound
-        console.log(id);//just for reference
-        console.log(location.toString());//just for reference
-        console.log(north_east.toString());//just for reference
-        console.log(south_west.toString());//just for reference
         nearbySearch(place);
     });
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    initAutocomplete();
-});
-
 function nearbySearch(place) {
+    clearMarkers();
+    var chose = document.getElementById('types');
+    var type = chose.options[chose.selectedIndex].value;
+    console.log(type);
     var search = {
         bounds: place.geometry.viewport,
-        types: ['lodging']
+        types: [type]
       };
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(search, callback);
     function callback(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-            }
+            createMarker(results);
         }
     }
 }
 
-function createMarker(place) {
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
+function createMarker(places) {
+    places.forEach(place => {
+        let marker = new google.maps.Marker({
+          position: place.geometry.location,
+          map: map,
+        });
+        markers.push(marker);
+        google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+        'Type: ' + place.types + '</div>');
+        infowindow.open(map, this);
+        });
     });
+}
 
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-      'Type: ' + place.types + '</div>');
-      infowindow.open(map, this);
-    });
+function clearMarkers() {
+    markers.forEach(function (m) { m.setMap(null); });
+    markers = [];
 }
